25cf1c455141b37d6dfdd1d0987c99a8c083e0cd
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2023-08-15 17:26:10 +0300

    Create basic.test.js

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,18 @@
+import orderByProps from '../basic';
+
+test('should sort correctly', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const sortoder = ['name', 'level'];
+  const result = orderByProps(obj, sortoder);
+  const expected = [
+    { key: 'name', value: 'мечник' },  
+    { key: 'level', value: 2 },  
+    { key: 'attack', value: 80 },  
+    { key: 'defence', value: 40 },  
+    { key: 'health', value: 10 },  
+  ];
+
+  expect(result).toEqual(expected);
+});
